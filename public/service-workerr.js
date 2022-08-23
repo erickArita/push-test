e27f83412f70132b4cd0bb134972c4df9bb9d4bc
Erick Arita <erick.emao@gmail.com>
2022-08-23 11:03:43 -0600

    add: notificación dinamica

--- public/service-workerr.js
+++ public/service-workerr.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
 self.addEventListener('push', e => {
 
-  const data = (e.data.json());
+  const data = e.data.json();
 
-  self.registration.showNotification(data.Title, {
-    body: data.Body + self.navigator.platform + self.navigator.userAgent,
-    icon: '/logo512.png'
-  })
+  self.registration.showNotification(data.title, { icon: '/logo512.png', ...data })
 })
+
+self.addEventListener('notificationclick', (event) => {
+
+  event.notification.close();
+
+  clients.openWindow(event.notification.data.url, '_blank');
+
+});
